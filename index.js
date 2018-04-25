8483db5ce5ed5b04d61a62416b6270b98c259433
Sourabh Saha <sourabhsaha16@gmail.com>
2018-04-25 03:59:54 +0000

    mvp created

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ var path = require("path");
 var bodyParser = require('body-parser');
 var collection = null;
 var db = null;
+var localtunnel = require('localtunnel');
+
+var tunnel = localtunnel(8080,{subdomain:'fantasyfifaworldcup2018'}, function(err, tunnel) {
+  console.log(tunnel.url);
+});
+
+tunnel.on('close', function() {
+    // tunnels are closed
+});
 
 app.use(express.static('public'));
 app.use(bodyParser.json()); // for parsing application/json
@@ -12,12 +21,12 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing
 
 app.get('/players',function(req,res){
   playerCollection = db.collection('players');
-  playerCollection.find({},{limit:100}).toArray(function(err,data){
+  playerCollection.find({},{limit:20}).toArray(function(err,data){
     res.send(data);
   });
 });
 
-app.get('/search/player/:type/:value',function(req,res){
+app.get('/search/player/all/:type/:value',function(req,res){
   var type = req.params.type;
   var value = req.params.value;
 
@@ -28,7 +37,7 @@ app.get('/search/player/:type/:value',function(req,res){
     query = {Name:{$regex:value}};
     break;
     case 'Rating':
-    query = {Rating:{$lt:Number(value)}};
+    query = {Rating:{$lte:Number(value)}};
     break;
     case 'Nationality':
     query = {Nationality:{$regex:value}};
@@ -44,6 +53,44 @@ app.get('/search/player/:type/:value',function(req,res){
 
 });
 
+app.get('/search/player/one/:position/:type/:value',function(req,res){
+  var type = req.params.type;
+  var value = req.params.value;
+  var position = req.params.position;
+  var query = {};
+
+  switch(type){
+    case 'Name':
+    query = {Name:{$regex:value},Preffered_Position:{$regex:position}};
+    break;
+    case 'Rating':
+    query = {Rating:{$lte:Number(value)},Preffered_Position:{$regex:position}};
+    break;
+    case 'Nationality':
+    query = {Nationality:{$regex:value},Preffered_Position:{$regex:position}};
+    break;
+  }
+  playerCollection = db.collection('players');
+  playerCollection.find(query,{limit:20}).toArray(function(err,data){
+    res.send(data);
+  });
+
+});
+
+app.get('/search/player/position/:position',function(req,res){
+  var position = req.params.position;
+  if(position=='CB1' || position=='CB2'){
+    position = 'CB';
+  }
+  var query = {Preffered_Position:{$regex:position}};
+
+  playerCollection = db.collection('players');
+  playerCollection.find(query,{limit:20}).toArray(function(err,data){
+    res.send(data);
+  });
+
+});
+
 app.post('/login', function (req, res) {
   authCollection = db.collection("users");
   authCollection.findOne({Username:req.body.user,Password:req.body.pwd},function(err,r){
@@ -51,8 +98,13 @@ app.post('/login', function (req, res) {
       res.send("error");
     }
     else{
-      if(r!==null)
-        res.send(r.Username);
+      if(r!==null){
+        var query = {'user':r.Username};
+        teamCollection = db.collection('teams');
+        teamCollection.find(query,{limit:20}).toArray(function(err,data){
+          res.send({Team:data,Username:r.Username});
+        });
+      }
       else {
         res.send("access_denied");
       }
@@ -67,7 +119,78 @@ app.post('/register', function (req, res) {
       res.send("error");
     }
     else{
-      res.send(String(r.insertedCount));
+      teamCollection = db.collection("teams");
+      teamCollection.insertOne({'user':req.body.user,'ST':'ST','LW':'LW','RW':'RW','LM':'LM','CM':'CM','RM':'RM','LB':'LB','CB1':'CB','CB2':'CB','RB':'RB','GK':'GK'},
+      function(err,r){
+        if(err){
+          res.send("error");
+        }
+        else{
+          res.send("done");
+        }
+      });
+    }
+  });
+});
+
+app.post('/add/player/',function(req,res){
+  teamCollection = db.collection("teams");
+  var pos = req.body.position;
+  var query = {};
+  switch(pos){
+    case 'ST':
+    query = {'ST':req.body.name};
+    break;
+    case 'LW':
+    query = {'LW':req.body.name};
+    break;
+    case 'RW':
+    query = {'RW':req.body.name};
+    break;
+    case 'LM':
+    query = {'LM':req.body.name};
+    break;
+    case 'CM':
+    query = {'CM':req.body.name};
+    break;
+    case 'RM':
+    query = {'RM':req.body.name};
+    break;
+    case 'LB':
+    query = {'LB':req.body.name};
+    break;
+    case 'CB1':
+    query = {'CB1':req.body.name};
+    break;
+    case 'CB2':
+    query = {'CB2':req.body.name};
+    break;
+    case 'RB':
+    query = {'RB':req.body.name};
+    break;
+    case 'GK':
+    query = {'GK':req.body.name};
+    break;
+  }
+  console.log(query,req.body);
+  teamCollection.updateOne({user:req.body.user},{$set:query},function(err,r){
+    if(err){
+      res.send("error");
+    }
+    else{
+      teamCollection.findOne({user:req.body.user},function(err,r){
+        if(err){
+          res.send("error");
+        }
+        else{
+          if(r!==null){
+            res.send(r);
+          }
+          else {
+            res.send("access_denied");
+          }
+        }
+      });
     }
   });
 });
--- public/core.js
+++ public/core.js
@@ -2,9 +2,10 @@ var app = angular.module('myApp',[]);
 app.controller('mainController',function($scope,$http){
 
   $scope.loggedInUser = "";
-
+  $scope.position = "";
   $scope.showView = 'login.html';
   $scope.login = false;
+  $scope.loggedInUuid = "";
 
   $scope.verify = function(username,password){
     var loginDetails = {'user':username,'pwd':password};
@@ -12,7 +13,8 @@ app.controller('mainController',function($scope,$http){
       if(res.data!=='access_denied'){
         $scope.showView = 'team.html';
         $scope.login = true;
-        $scope.loggedInUser = res.data;
+        $scope.teamData = res.data.Team[0];
+        $scope.loggedInUser = res.data.Username;
       }
     });
   };
@@ -29,10 +31,16 @@ app.controller('mainController',function($scope,$http){
   }
 
 
-  $scope.changeView = function(viewName){
+  $scope.changeView = function(viewName,pos){
     $scope.showView = viewName;
-    if(viewName=='players.html'){
-      $http.get("http://192.168.33.123:8080/players").then(function(res){
+    if(viewName==='players.html'){
+      $http.get("http://fantasyfifaworldcup2018.localtunnel.me/players").then(function(res){
+        $scope.playerData = res.data;
+      });
+    }
+    else if(viewName==='choosePlayer.html'){
+      $scope.position = pos;
+      $http.get("http://fantasyfifaworldcup2018.localtunnel.me/search/player/position/"+$scope.position).then(function(res){
         $scope.playerData = res.data;
       });
     }
@@ -40,8 +48,24 @@ app.controller('mainController',function($scope,$http){
 
   $scope.search = function(type,value){
     if(value.match(/^[a-z0-9]+$/i) !== null)
-      $http.get("http://192.168.33.123:8080/search/player/"+type+"/"+value).then(function(res){
+      $http.get("http://fantasyfifaworldcup2018.localtunnel.me/search/player/all/"+type+"/"+value).then(function(res){
           $scope.playerData = res.data;
       });
   };
+
+  $scope.searchPlayer = function(type,value){
+    if(value.match(/^[a-z0-9]+$/i) !== null)
+      $http.get("http://fantasyfifaworldcup2018.localtunnel.me/search/player/one/"+$scope.position+"/"+type+"/"+value).then(function(res){
+          $scope.playerData = res.data;
+      });
+  };
+
+  $scope.addPlayer = function(name,nationality,rating){
+    var combinedString = name+"#"+nationality+"#"+rating;
+    var addPlayerDetails = {'user':$scope.loggedInUser,'name':combinedString,'position':$scope.position};
+    $http.post('/add/player',JSON.stringify(addPlayerDetails)).then(function(res){
+      $scope.teamData = res.data;
+      $scope.showView = 'team.html';
+    });
+  }
 });
